d0bf70c934a439974f1d1da6f4d42802192bc703
Sota Makino <sm96kyoto@gmail.com>
2020-02-12 08:35:30 +0900

    feat: add TEA

--- src/main.elm
+++ src/main.elm
@@ -1,7 +1,43 @@
-module Main exposing (main)
+module Main exposing (Model, Msg, init, update, view)
 
-import Html exposing (text)
+import Browser
+import Html exposing (..)
 
 
+main : Program () Model Msg
 main =
-    text "Hello !"
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    { id : Int, value : Int }
+
+
+init : Model
+init =
+    { id = 1, value = 10 }
+
+
+type Msg
+    = Msg1
+    | Msg2
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Msg1 ->
+            model
+
+        Msg2 ->
+            model
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "New Sandbox" ]
