b33bda56e8ad76ccf4cc231053fc5637f03d5532
Sota Makino <sm96kyoto@gmail.com>
2020-03-01 18:01:55 +0900

    feat: add randomizer

--- src/main.elm
+++ src/main.elm
@@ -4,6 +4,8 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Random exposing (Seed, generate)
+import Random.List exposing (shuffle)
 import SimpleGraph exposing (Option(..), barChart, lineChart)
 
 
@@ -28,7 +30,12 @@ barGraphAttributes =
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
@@ -39,24 +46,39 @@ type alias Model =
     { barList : List Int }
 
 
-init : Model
-init =
-    { barList = List.range 1 15 }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { barList = List.range 1 15 }
+    , Cmd.none
+    )
 
 
 type Msg
-    = NewValue
+    = Randomize
+    | RandomizedList (List Int)
 
 
 
 --UPDATE
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NewValue ->
-            model
+        Randomize ->
+            ( model, generate RandomizedList (shuffle model.barList) )
+
+        RandomizedList randomizedList ->
+            ( { model | barList = randomizedList }, Cmd.none )
+
+
+
+--SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
@@ -73,4 +95,5 @@ view model =
             ]
             [ text "Comparison Sorting Algorithms" ]
         , div [] [ barChart barGraphAttributes (floatedList model.barList) ]
+        , div [] [ button [ onClick Randomize ] [ text "Randomize Array" ] ]
         ]
